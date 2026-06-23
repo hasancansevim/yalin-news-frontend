824d97ea3db3fdae8d31db3ae95daac91cbc8138
Hasan Can Sevim <shasancan0@gmail.com>
2026-06-23 23:43:14 +0300

    Perf: Optimize TBT and Hydration with defer and GPU acceleration

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,8 @@ module.exports = {
       },
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          '0%': { transform: 'translateX(100%) translateZ(0)' },
+          '100%': { transform: 'translateX(-100%) translateZ(0)' },
         }
       },
       animation: {
